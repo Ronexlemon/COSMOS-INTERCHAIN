497fb72eca60999984d2046f194e969fb579b8d2
Ronex Ondimu <ronexlemon@gmail.com>
2023-11-05 22:42:41 +0300

    Multiple Variable Declaration

--- go-learn/index.go
+++ go-learn/index.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 )
+// func add(num1 int,num2 int){
+// 	sum:= num1 +num2;
+// 	return;
 
+// }
 func main() {
 	var num int = 2
 	x := "yoolow"
@@ -14,6 +18,8 @@ func main() {
 	//value assignment after declaration
 	var interchain string;
 	interchain= "COSMOS";
+	//Multiple Variable Declaration
+var aa,bb,c,dd int =1,2,3,4
 	fmt.Println("Hellow Interchain")
 	fmt.Println(num);
 	fmt.Println(x);
@@ -21,4 +27,5 @@ func main() {
 	fmt.Println(b);
 	fmt.Println(d);
 	fmt.Println(interchain);
+	fmt.Println(aa,bb,c,dd)
 }
