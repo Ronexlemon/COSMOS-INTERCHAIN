b68a1b9092a785bf77fdbf4b66f6081f594b6625
Ronex Ondimu <ronexlemon@gmail.com>
2023-11-05 22:34:20 +0300

    variable dec without initial value

--- go-learn/index.go
+++ go-learn/index.go
@@ -7,9 +7,14 @@ import (
 func main() {
 	var num int = 2
 	x := "yoolow"
+	//varibale without initial value
 	var name string
-	
+	var b bool
+	var d float32
 	fmt.Println("Hellow Interchain")
 	fmt.Println(num);
 	fmt.Println(x);
+	fmt.Println(name);
+	fmt.Println(b);
+	fmt.Println(d);
 }
