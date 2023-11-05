3c466925fb7a60957e6263d69ddcdc8e24af1e4f
Ronex Ondimu <ronexlemon@gmail.com>
2023-11-05 22:32:57 +0300

    syntax

--- go-learn/index.go
+++ go-learn/index.go
@@ -1,1 +1,15 @@
-//STARTING
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num int = 2
+	x := "yoolow"
+	var name string
+	
+	fmt.Println("Hellow Interchain")
+	fmt.Println(num);
+	fmt.Println(x);
+}
