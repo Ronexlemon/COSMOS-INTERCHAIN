7572fa1aea5ed4aa047703ad699dc8797d93ab52
Ronex Ondimu <ronexlemon@gmail.com>
2023-11-06 13:53:31 +0300

    array

---
+++ go-learn/array.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var array_num = [...]int{1,2,3,4,5,6}
+var array_names =[10]string{"lemon","lemon","lemon"}
+
+func array() {
+	// Add code for the array function here if needed.
+	nameit_array:=[...]int{1,3,4,5,6,6}
+	fmt.Println("pass it")
+	fmt.Println(array_num);
+	fmt.Println(array_names);
+	fmt.Println(nameit_array);
+}
--- go-learn/index.go
+++ go-learn/index.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 )
-func add(num1 int,num2 int)(int){
-	sum:= num1 +num2;
-	return sum;
+
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+	return sum
 
 }
 func main() {
+	array()
 	var num int = 2
 	x := "yoolow"
 	//varibale without initial value
@@ -16,20 +18,20 @@ func main() {
 	var b bool
 	var d float32
 	//value assignment after declaration
-	var interchain string;
-	interchain= "COSMOS";
+	var interchain string
+	interchain = "COSMOS"
 	//Multiple Variable Declaration
-var aa,bb,c,dd int =1,2,3,4
-//constants
-const pie int16=60
+	var aa, bb, c, dd int = 1, 2, 3, 4
+	// constants
+	const pie int16 = 60
 	fmt.Println("Hellow Interchain")
-	fmt.Println(num);
-	fmt.Println(x);
-	fmt.Println(name);
-	fmt.Println(b);
-	fmt.Println(d);
-	fmt.Println(interchain);
-	fmt.Println(aa,bb,c,dd)
-	fmt.Println(pie);
-	fmt.Println(add(3,4))
+	fmt.Println(num)
+	fmt.Println(x)
+	fmt.Println(name)
+	fmt.Println(b)
+	fmt.Println(d)
+	fmt.Println(interchain)
+	fmt.Println(aa, bb, c, dd)
+	fmt.Println(pie)
+	fmt.Println(add(3, 4))
 }
